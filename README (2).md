9ac5eda56069caa5d4ebfb6cdaf83526290c5da8
Blizgo <146516043+muldimedia@users.noreply.github.com>
2025-02-08 16:03:33 +0200

    Fix Double
    Fixed a stupid double folder mistake

--- README (2).md
+++
@@ -1,4 +0,0 @@
-# My Portfolio
-An attempt at creating a mini portfolio/website.
-It's always an on progress project.
-Adding and trying new things as soon as I learn them.
--- script (2).js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Mobile Burger Menu
-  const burgerIcon = document.getElementById("burger-icon");
-  const navLinksMobile = document.getElementById("nav-links-mobile");
-
-  burgerIcon.addEventListener("click", function () {
-    navLinksMobile.classList.toggle("show");
-  });
-});
